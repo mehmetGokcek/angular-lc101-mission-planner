99a246720fa7135c756250ce4049e31d47900fbf
Mehmet Gokcek <gokcek50@gmail.com>
2020-09-21 02:01:01 -0500

    all tasks completed except for bonus tasks

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -25,5 +25,28 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+  addCrewMember(candidate: object){
+    if(!this.crew.includes(candidate) && this.crew.length < 3 ){
+    this.crew.push(candidate);
+    this.inCrew = true;
+    }
+    else if(this.crew.includes(candidate)){
+      let index = this.crew.indexOf(candidate);
+      this.crew.splice(index,1);
+      this.inCrew = false;
+    }
+    else{
+      this.inCrew = false
+    }
+  }
+  
+crewIncluded(candidate:object):boolean{
+  if(this.crew.includes(candidate)){
+  return true;
+  }
+  else {
+    return false;
+    }
+  }
 
 }
